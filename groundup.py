24bdeb6bd04ee8e1edfa66887d8ddbbda9d42d22
Rithwik Sudharsan <indy7500@gmail.com>
2019-09-23 00:00:27 -0700

    Update groundup.py

--- groundup.py
+++ groundup.py
@@ -21,6 +21,23 @@ def roll_dice(num_rolls, dice=six_sided):
         return 1
     return score
 
+def make_averaged(fn, num_samples=1000):
+    """Return a function that returns the average value of FN when called.
+    To implement this function, you will have to use *args syntax, a new Python
+    feature introduced in this project.  See the project description.
+    >>> dice = make_test_dice(4, 2, 5, 1)
+    >>> averaged_dice = make_averaged(dice, 1000)
+    >>> averaged_dice()
+    3.0
+    """
+    # BEGIN PROBLEM 8
+    def average(*args):
+        value = 0
+        for x in range(num_samples):
+            value += fn(*args)
+        return value / num_samples
+    return average
+    # END PROBLEM 8
 
 def free_bacon(score):
     assert score < 100, 'The game should be over.'
@@ -38,7 +55,7 @@ def take_turn(num_rolls, opponent_score, dice=six_sided):
     assert opponent_score < 100, 'The game should be over.'
 
     if num_rolls != 0:
-        return roll_dice(num_rolls, dice)
+        return make_averaged(roll_dice)(num_rolls, dice)
     else:
         return free_bacon(opponent_score)
 
@@ -72,24 +89,6 @@ def complete_turn(num_rolls, score0, score1, prev_num_rolls, dice=six_sided):
     prev_num_rolls = new0
     return score0, score1, prev_num_rolls
 
-def make_averaged(fn, num_samples=1000):
-    """Return a function that returns the average value of FN when called.
-    To implement this function, you will have to use *args syntax, a new Python
-    feature introduced in this project.  See the project description.
-    >>> dice = make_test_dice(4, 2, 5, 1)
-    >>> averaged_dice = make_averaged(dice, 1000)
-    >>> averaged_dice()
-    3.0
-    """
-    # BEGIN PROBLEM 8
-    def average(*args):
-        value = 0
-        for x in range(num_samples):
-            value += fn(*args)
-        return value / num_samples
-    return average
-    # END PROBLEM 8
-
 # V1 Features: score, opponent
 # V2 Features: free_bacon(opponent)
 # V3 Features: swapmult(opponent)
@@ -100,12 +99,12 @@ gamma = 0.75
 Q = np.zeros((101, 101, 11))
 # Need to convert this to a dictionary at the end
 
-def update(score0, score1, action): # this is unfinished
-    # Update q values (this is unfinished)
-    Q[score0, score1, action] = Q[score0, score1, action] + (lr * (1 + gamma * np.max(Q[new_state, :])) - Q[state, action])
+def update(score0, score1, action, nextscore0, nextscore1):
+    Q[score0, score1, action] = Q[score0, score1, action] + (lr * (1 + gamma * np.max(Q[nextscore0, nextscore1, :]) - Q[score0, score1, action]))
 
 for i in range(games):
-    record0, record1 = [], []
+    record0 = []
+    record1 = []
     score0, score1, prev0, prev1 = 0, 0, 0, 0
     epsilon = 0.2
 
@@ -120,11 +119,11 @@ for i in range(games):
             score0, score1, prev0 = complete_turn(qrolls, score0, score1, prev0)
 
         if score0>=100:
-            for i in record0:
-                update(i[0],i[1],i[2])
+            for i in range(len(record0)-1):
+                update(record0[i][0],record0[i][1],record0[i][2], record0[i+1][0], record0[i+1][1])
         elif score1>=100:
-            for i in record1:
-                update(i[0],i[1],i[2])
+            for i in range(len(record1)-1):
+                update(record1[i][0],record1[i][1],record1[i][2], record1[i+1][0], record1[i+1][1])
 
         if random.uniform(0, 1) < epsilon or score1==0:
             randrolls = random.randint(0, 10)
@@ -136,8 +135,8 @@ for i in range(games):
             score0, score1, prev01 = complete_turn(qrolls, score0, score1, prev1)
 
         if score0>=100:
-            for i in record0:
-                update(i[0],i[1],i[2])
-        elif score1>=100:
-            for i in record1:
-                update(i[0],i[1],i[2])
+            for i in range(len(record0) - 1):
+                update(record0[i][0], record0[i][1], record0[i][2], record0[i + 1][0], record0[i + 1][1])
+        elif score1 >= 100:
+            for i in range(len(record1) - 1):
+                update(record1[i][0], record1[i][1], record1[i][2], record1[i + 1][0], record1[i + 1][1])
